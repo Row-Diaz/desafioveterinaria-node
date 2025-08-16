92f73ab365d8ffd0f9730f63126c723a24ff68ad
Row <axlrower@gmail.com>
2025-08-16 12:46:09 -0400

    desarrollo de la primera parte del archivo index

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const fs = require('fs');
 const { registrar, leer } = require('./operaciones.js');
 
+
+if(solicitud === "registrar"){
+    registrar(nombre, edad, animal, color, enfermedad)
+}
+else if (solicitud === "leer"){
+    leer()
+}
+else{
+    console.log("No se ha podido registrar a tu mascota, asegurate de ingresar bien los datos")
+}
