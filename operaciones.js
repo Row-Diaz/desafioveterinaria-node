003cad26d68a4f1963eeb519341031e61ed26cd1
Row <axlrower@gmail.com>
2025-08-16 12:21:33 -0400

    creacion de la funcion leer

--- operaciones.js
+++ operaciones.js
@@ -1,16 +1,26 @@
-const fs = require('fs');
+const fs = require("fs");
 
 function registrar(nombre, edad, animal, color, enfermedad) {
-    const registro = {
-        nombre: nombre,
-        edad: edad,
-        animal: animal,
-        color: color,
-        enfermedad: enfermedad
-    };
+  const registro = {
+    nombre: nombre,
+    edad: edad,
+    animal: animal,
+    color: color,
+    enfermedad: enfermedad,
+  };
 
-   const data = fs.readFileSync('citas.json', 'utf8');
-   const cita = JSON.parse(data);
-   cita.push(registro);
-   fs.writeFileSync('citas.json', JSON.stringify(cita));
+  const data = fs.readFileSync("citas.json", "utf8");
+  const cita = JSON.parse(data);
+  cita.push(registro);
+  fs.writeFileSync("citas.json", JSON.stringify(cita));
 }
+
+function leer(){
+    const formularios = fs.readFileSync('citas.json', 'utf8')
+    JSON.parse(formularios).forEach(mascota => {
+        console.log(mascota)
+    });
+
+}
+
+module.exports= { registrar, leer };
